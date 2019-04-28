066af6139c9dc3b07bc8e54aa6f90f1d3b4b4c53
jime.zhu <nodbrag@sina.com>
2019-04-28 16:01:41 +0800

    add vue-demo

---
+++ vue-test/import-demo/common.js
@@ -0,0 +1,7 @@
+let common={
+    name:"test"
+};
+common.get=function () {
+    return "aaaa"
+};
+export default  common;
---
+++ vue-test/单页面组件/home.vue
@@ -0,0 +1,9 @@
+<template>
+    <p>Welcome home</p>
+</template>
+
+<script type="module">
+    export default {
+        name:"test"
+    }
+</script>
