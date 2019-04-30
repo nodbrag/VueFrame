82853d67b5559fb667cd4f8bbfd976f5933c2a6e
jime.zhu <nodbrag@sina.com>
2019-04-30 15:40:04 +0800

    update vuex-test

--- vuex-test/src-todos/main.js
+++ vuex-test/src-todos/main.js
@@ -3,21 +3,15 @@
  */
 import Vue from 'vue'
 import App from './App.vue'
-import store from './vuex/store'
+import store from './store'
 
+import './base.css'
 
 // 创建vm
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render: h => h(App),
-  store
-})
-
-/*
-new Vue({
-  el: '#app',
   components: {App}, // 映射组件标签
   template: '<App/>', // 指定需要渲染到页面的模板
-  store
-})*/
+  store // 所有的组件对象都多了一个属性: $store(值就是store对象)
+})
